28e951f8786a10805937dccacd463ecbace82b55
SantiagoIturriVargasUCB <santiago.iturri@ucb.edu.bo>
2024-08-20 09:42:06 -0400

    Implementar funcionalidad de multiplicar

---
+++ src/sumador-multiplicador/functions.js
@@ -0,0 +1,2 @@
+export const sumar = (a, b) => { return a + b }
+export const multiplicar = (a, b) => { return a * b }
--- src/sumador-multiplicador/presenter.js
+++ src/sumador-multiplicador/presenter.js
@@ -1,15 +1,31 @@
-import sumar from "./sumador";
+import { sumar, multiplicar } from "./functions";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+const firstSum = document.querySelector("#primer-numero");
+const secondSum = document.querySelector("#segundo-numero");
+const firstMult = document.querySelector("#primer-numero-mult");
+const secondMult = document.querySelector("#segundo-numero-mult")
+const formSuma = document.querySelector("#sumar-form");
+const formMultiplicar = document.querySelector("#multiplicar-form");
+const divSuma = document.querySelector("#resultado-suma-div");
+const divMultiplicar = document.querySelector("#resultado-multiplicacion-div")
 
-form.addEventListener("submit", (event) => {
+formSuma.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const firstNumberSum = Number.parseInt(firstSum.value);
+  const secondNumberSum = Number.parseInt(secondSum.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  divSuma.innerHTML = "<p>" + sumar(firstNumberSum, secondNumberSum) + "</p>";
 });
+
+formMultiplicar.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumberFor = Number.parseInt(firstMult.value);
+  const secondNumberFor = Number.parseInt(secondMult.value);
+
+  divMultiplicar.innerHTML = "<p>" + multiplicar(firstNumberFor, secondNumberFor) + "</p>";
+});
+
+
+
