c56fadddcd2c440893950b120e1bb6657133b06b
SantiagoIturriVargasUCB <santiago.iturri@ucb.edu.bo>
2024-08-20 10:23:28 -0400

    implementar funcionalidad de saludar por nombre

---
+++ src/greeter-bot/javascript/displayGreetByName.js
@@ -0,0 +1,13 @@
+import { languageSpanish } from './language';
+
+const greetButton = document.getElementById('greet-button-by-name');
+const nameInput = document.getElementById('name-input');
+
+greetButton.addEventListener('click', () => {
+    const name = nameInput.value.trim();  
+    if (name) {
+        let greeting = `Hello, ${name}!`;
+        if (languageSpanish.checked) greeting = `Hola, ${name}`;
+        alert(greeting);
+    }
+});
