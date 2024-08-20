6ba7f9481cea2e51e48c2e454965f85df3c5893e
SantiagoIturriVargasUCB <santiago.iturri@ucb.edu.bo>
2024-08-20 19:57:11 -0400

    some changes

--- src/greeter-bot/javascript/displayGreetByName.js
+++ src/greeter-bot/javascript/displayGreetByName.js
@@ -6,7 +6,7 @@ const nameInput = document.getElementById('name-input');
 greetButton.addEventListener('click', () => {
     const name = nameInput.value.trim();  
     if (name) {
-        let greeting = `Hello, ${name}!`;
+        let greeting = `Hello, ${name}`;
         if (languageSpanish.checked) greeting = `Hola, ${name}`;
         alert(greeting);
     }
