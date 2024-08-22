41ccdf1b3d6b922a44a76b0289f264e7cdbecc1d
SantiagoIturriVargasUCB <santiago.iturri@ucb.edu.bo>
2024-08-22 10:13:47 -0400

    implementacion fizzbuzz a html

--- src/fizzbuzz-work/fizzbuzz.js
+++ src/fizzbuzz-work/fizzbuzz.js
@@ -15,3 +15,15 @@ export const generarSecuencia = (limite) => {
     }
     return res;
 }
+
+document.getElementById('generate').addEventListener('click', () => {
+    const limite = document.getElementById('limite').value;
+    const resultDiv = document.getElementById('result');
+
+    if (limite && limite > 0) {
+        const sequence = generarSecuencia(limite);
+        resultDiv.innerHTML = sequence.join(', ');
+    } else {
+        resultDiv.innerHTML = 'Please enter a valid number';
+    }
+});
