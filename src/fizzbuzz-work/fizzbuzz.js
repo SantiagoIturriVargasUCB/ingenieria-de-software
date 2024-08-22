b520883565cb8e47a69637639adaae9441efcc97
SantiagoIturriVargasUCB <santiago.iturri@ucb.edu.bo>
2024-08-22 09:46:34 -0400

    segunda funcionalidad implenetada

--- src/fizzbuzz-work/fizzbuzz.js
+++ src/fizzbuzz-work/fizzbuzz.js
@@ -1,3 +1,4 @@
 export const fizzbuzz = (numero) => {
     if( numero < 3) return numero
+    if( numero === 3 ) return 'Fizz'
 }
--- src/fizzbuzz-work/fizzbuzz.test.js
+++ src/fizzbuzz-work/fizzbuzz.test.js
@@ -3,3 +3,7 @@ import { fizzbuzz } from "./fizzbuzz";
 test('debe retornar "1" cuando se pasa 1', () => {
     expect(fizzbuzz(1)).toBe(1);
 });
+
+test('debe retornar fizz para 3', () => { 
+    expect(fizzbuzz(3)).toBe('Fizz');
+});
