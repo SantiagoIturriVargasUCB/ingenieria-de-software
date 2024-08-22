6ad0c6725995a0c4f0d4b472d23402b71662b4fd
SantiagoIturriVargasUCB <santiago.iturri@ucb.edu.bo>
2024-08-22 10:09:33 -0400

    funcionalidad de secuencia funcionando

--- src/fizzbuzz-work/fizzbuzz.js
+++ src/fizzbuzz-work/fizzbuzz.js
@@ -4,3 +4,14 @@ export const fizzbuzz = (numero) => {
     else if (numero % 5 === 0) return 'Fizz';
     else return numero;
 }
+
+export const generarSecuencia = (limite) => {
+    const res = [];
+    for (let i = 1; i <= limite; i++) {
+        if (i % 3 === 0 && i % 5 === 0) res.push('FizzBuzz');
+        else if (i % 3 === 0) res.push('Fizz');
+        else if (i % 5 === 0) res.push('Buzz')
+        else res.push(i);
+    }
+    return res;
+}
--- src/fizzbuzz-work/fizzbuzz.test.js
+++ src/fizzbuzz-work/fizzbuzz.test.js
@@ -1,30 +1,30 @@
-import { fizzbuzz } from "./fizzbuzz";
+import { fizzbuzz, generarSecuencia } from "./fizzbuzz";
 
 test('debe retornar "1" cuando se pasa 1', () => {
     expect(fizzbuzz(1)).toBe(1);
 });
 
-test('debe retornar fizz para 3', () => { 
+test('debe retornar fizz para 3', () => {
     expect(fizzbuzz(3)).toBe('Fizz');
 });
 
-test('debe retornar fizz para multiplos de 3', () => { 
+test('debe retornar fizz para multiplos de 3', () => {
     expect(fizzbuzz(3)).toBe('Fizz');
     expect(fizzbuzz(6)).toBe('Fizz');
     expect(fizzbuzz(9)).toBe('Fizz');
 });
 
-test('debe retornar fizz para el 5', () => { 
+test('debe retornar fizz para el 5', () => {
     expect(fizzbuzz(5)).toBe('Fizz');
 });
 
-test('debe retornar fizz mutiplos de 5', () => { 
+test('debe retornar fizz mutiplos de 5', () => {
     expect(fizzbuzz(5)).toBe('Fizz');
     expect(fizzbuzz(10)).toBe('Fizz');
     expect(fizzbuzz(25)).toBe('Fizz');
 });
 
-test('debe retornar fizzbuzz para el 15', () => { 
+test('debe retornar fizzbuzz para el 15', () => {
     expect(fizzbuzz(15)).toBe('FizzBuzz');
 });
 
@@ -32,5 +32,11 @@ test('debe retornar fizbuzz para mutiplos de 3 y de 5', () => {
     expect(fizzbuzz(15)).toBe('FizzBuzz');
     expect(fizzbuzz(30)).toBe('FizzBuzz');
     expect(fizzbuzz(60)).toBe('FizzBuzz');
-    
+});
+
+
+test('debe retornar una secuencia de números con "Fizz", "Buzz" y "FizzBuzz"', () => {
+    const limite = 15;
+    const esperado = [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz'];
+    expect(generarSecuencia(limite)).toEqual(esperado);
 });
