4681e30de7e102b534633f425ba54d8efcfb73db
SantiagoIturriVargasUCB <santiago.iturri@ucb.edu.bo>
2024-08-25 20:34:56 -0400

    some changes

--- src/tdd-leap-year/isLeap.js
+++ src/tdd-leap-year/isLeap.js
@@ -1,20 +1,12 @@
 const isLeapYear = year => {
-    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) 
-        return true;
-    else 
-        return false;
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0)  return true;
+    else  return false;
 }
 
-const cheepYear = () => {
-    const year = document.getElementById('yearInput').value;
-    if (year === '') {
-        alert('Por favor, ingresa un año.');
-        return;
-    }
+const year = document.getElementById('yearInput').value;
 
-    const leapYear = isLeapYear(parseInt(year));
-    if (leapYear) 
-        alert(`El año ${year} es bisiesto.`);
-    else
-        alert(`El año ${year} no es bisiesto.`);
-}
+const leapYear = isLeapYear(parseInt(year));
+if (leapYear) alert(`El año ${year} es bisiesto.`);
+else alert(`El año ${year} no es bisiesto.`);
+
+document.getElementById("verifyButton").addEventListener("click", checkLeapYear);
