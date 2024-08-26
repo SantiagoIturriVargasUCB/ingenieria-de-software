b999f1caef765e2ce8000f7f0f180d6f9c329b1f
SantiagoIturriVargasUCB <santiago.iturri@ucb.edu.bo>
2024-08-25 20:56:34 -0400

    change DOM function

--- src/tdd-leap-year/isLeap.js
+++ src/tdd-leap-year/isLeap.js
@@ -3,14 +3,11 @@ export const isLeapYear = year => {
     else return false;
 };
 
-const checkLeapYear = () => {
+document.getElementById("verifyButton").addEventListener("click", () => {
     const year = document.getElementById('yearInput').value;
-
     const leapYear = isLeapYear(parseInt(year));
     if (leapYear) alert(`El año ${year} es bisiesto.`);
     else alert(`El año ${year} no es bisiesto.`);
-    
-    document.getElementById("verifyButton").addEventListener("click", checkLeapYear);
-}
+});
 
 
